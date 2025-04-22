148ba1f66ba48819fd18701d8e99496f72496142
shivamtechstack <devshivamyadav1604@gmail.com>
2025-04-22 17:44:19 +0530

    feat: Implement SplashScreenActivity and migrate to OnBoarding/MainActivity
    This commit introduces a new `SplashScreenActivity` that handles the app's launch and determines whether to navigate to `OnBoardingActivity` (for first-time users) or `MainActivity` (for returning users).
    
    **Key changes:**
    
    -   **Added `SplashScreenActivity.kt`:** Implemented a `SplashScreenActivity` that uses `SharedPreferencesHelper` to check if the user is launching the app for the first time.
    -   **Conditional Navigation:**
        -   If it's the first launch, the app navigates to `OnBoardingActivity`.
        -   Otherwise, it navigates to `MainActivity`.
    -    **Delay on splashscreen**: added a 1 second delay in order to show the splashscreen before going to the next activity.
    -   **finish() after intent:** Added a finish() after the start of each activity to avoid the user to go back to the SplashScreenActivity.
    - **Updated  dependencies:** Updated dependencies to implement splashscreen.

--- app/src/main/java/com/sycodes/careerbot/SplashScreenActivity.kt
+++ app/src/main/java/com/sycodes/careerbot/SplashScreenActivity.kt
@@ -3,6 +3,7 @@ package com.sycodes.careerbot
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -30,13 +31,17 @@ class SplashScreenActivity : AppCompatActivity() {
         sharedPreferencesHelper = SharedPreferencesHelper(this)
         val isFirstLaunch = sharedPreferencesHelper.isFirstTimeUser()
 
-        if (isFirstLaunch){
-            var intent = Intent(this, OnBoardingActivity::class.java)
-            startActivity(intent)
-            finish()
-        }else{
-            var intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-        }
+        Handler().postDelayed({
+
+            if (isFirstLaunch) {
+                var intent = Intent(this, OnBoardingActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                var intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        },1000)
     }
 }
